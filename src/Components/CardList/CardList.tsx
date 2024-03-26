4faca3458af097dda4c5d2beefb61280da50f66b
Denis Sitko <Denis.Sitko@aspose.com>
2024-03-26 21:21:35 +0200

    Add .env file pattern, Search component, axios lib, request to Fin Marketing API

--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
-        <Card companyName='Apple1' ticker='AAPL' price={1}/>
-        <Card companyName='Apple2' ticker='AAPL' price={2}/>
-        <Card companyName='Apple3' ticker='AAPL' price={3}/>
+      <Card companyName='Apple1' ticker='AAPL' price={1} />
+      <Card companyName='Apple2' ticker='AAPL' price={2} />
+      <Card companyName='Apple3' ticker='AAPL' price={3} />
     </div>
   )
 }
