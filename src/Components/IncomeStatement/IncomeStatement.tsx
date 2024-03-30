7bb98ebb6380c206f9f4031e50323ec161a05439
Denis Sitko <Denis.Sitko@aspose.com>
2024-03-30 15:02:17 +0200

    Add new comps: Sidebar, Tiles, Company related, Table, new routes

---
+++ src/Components/IncomeStatement/IncomeStatement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const IncomeStatement = (props: Props) => {
+  return (
+    <div>IncomeStatements</div>
+  )
+}
+
+export default IncomeStatement
---
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+    <h1>StockApp Design Page</h1>
+    <h2>Design settings</h2>
+    <Table />
+    </>
+  )
+}
+
+export default DesignPage
