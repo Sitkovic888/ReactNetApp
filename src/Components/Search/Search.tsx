02f44d7d69f019e8a9d35b0e4550e72089ba521c
Denis Sitko <Denis.Sitko@aspose.com>
2024-12-14 18:40:44 +0200

    Use debounce in Search comp

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,30 +1,41 @@
-import React, { ChangeEvent, MouseEvent, SyntheticEvent, FormEvent } from 'react'
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import { useDebounce } from "use-debounce";
 
 interface Props {
-	onSearchSubmit: (e: SyntheticEvent) => void;
-	search: string | undefined;
-	handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleSearchChange: (e: string) => void;
 }
 
-const Search: React.FC<Props> = ({ onSearchSubmit, search, handleSearchChange }: Props): JSX.Element => {
-	return (
-		<section className="relative bg-gray-100">
-			<div className="max-w-4xl mx-auto p-6 space-y-6">
-				<form
-					className="form relative flex flex-col w-full p-10 space-y-4 bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
-					onSubmit={onSearchSubmit}
-				>
-					<input
-						className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
-						id="search-input"
-						placeholder="Search companies"
-						value={search}
-						onChange={handleSearchChange}
-					></input>
-				</form>
-			</div>
-		</section>
-	)
-}
+const Search: React.FC<Props> = ({
+  handleSearchChange,
+}: Props): JSX.Element => {
+  const [text, setText] = useState("");
+  const [debouncedValue] = useDebounce(text, 500);
+
+  useEffect(() => {
+    handleSearchChange(debouncedValue);
+  }, [debouncedValue]);
+
+  return (
+    <section className="relative bg-gray-100">
+      <div className="max-w-4xl mx-auto p-6 space-y-6">
+        <form
+          className="form relative flex flex-col w-full p-10 space-y-4 bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+        >
+          <input
+            className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
+            id="search-input"
+            placeholder="Search companies"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          ></input>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Search;
--- src/Pages/SearchPage/SearchPage.tsx
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -14,19 +14,20 @@ import {
 import { toast } from "react-toastify";
 import AddPortfolio from "../../Components/Portfolio/AddPortfolio/AddPortfolio";
 
-type Props = {};
-
-const SearchPage = (props: Props) => {
-  const [search, setSearch] = useState<string>("");
+const SearchPage = () => {
   const [portfolioValues, setPortfoliaValues] = useState<PortfolioGet[] | null>(
     []
   );
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
 
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
+  const handleSearchChange = async (searchQuery: string) => {
+    const result = await searchCompanies(searchQuery);
+    if (typeof result == "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
   };
 
   useEffect(() => getPortfolio(), []);
@@ -72,23 +73,9 @@ const SearchPage = (props: Props) => {
       );
   };
 
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    if (typeof result == "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-
-    console.log(searchResult);
-  };
-
   return (
     <>
       <Search
-        onSearchSubmit={onSearchSubmit}
-        search={search}
         handleSearchChange={handleSearchChange}
       />
       <ListPortfolio
